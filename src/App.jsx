4a207d1a2531249212bf7e10102ff1863b15e632
Tortugon09 <axelramos001@gmail.com>
2022-12-07 05:07:11 -0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
 import {Home} from "./components/Layouts/Home";
+import {NavBar} from "./components/Header/NavBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Products} from "./components/Layouts/Products";
 import {CartProvider} from "./components/CartContext";
 import {Shipping} from "./components/Layouts/Shipping";
 import {Clients} from "./components/Layouts/Clients";
+import Login from "./components/Layouts/Login.jsx";
 
 function App() {
 
@@ -15,7 +19,8 @@ function App() {
               <BrowserRouter>
                   <CartProvider>
                   <Routes>
-                      <Route path={"/"} element={<Home></Home>}></Route>
+                      <Route path={"/"} element={<Login></Login>}></Route>
+                      <Route path={"/Home"} element={<Home></Home>}></Route>
                       <Route path={"/Products"} element={<Products></Products>}></Route>
                       <Route path={"/Shipping"} element={<Shipping></Shipping>}></Route>
                       <Route path={"/Client"} element={<Clients></Clients>}></Route>
