c43a4fa12470c41da662bafc9bba0325482750b0
Tortugon09 <axelramos001@gmail.com>
2022-12-06 18:14:21 -0600

    add

---
+++ src/components/Body/BodyHome/Grafica.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "../../Layouts/Home.jsx";
+import {NavBar} from "../../Header/NavBar.jsx";
+
+
+export function Grafica() {
+
+
+
+    return (
+        <>
+
+        </>
+    )
+}
